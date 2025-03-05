5e338fd5e5fc09327116b5b479addcbbe3776ad8
Pedro Barros <am.pedrobarros@outlook.com>
2025-03-05 15:22:38 -0300

    feat: turn login page into welcome public page

---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ClientesService } from '../../services/clientes.service';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: false,
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss',
+})
+export class WelcomeComponent {
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private clientesService: ClientesService
+  ) {
+    this.form = this.fb.group({
+      username: new FormControl<string | null>(null, {
+        validators: Validators.required,
+      }),
+    });
+  }
+
+  signIn() {
+    if (this.form.invalid) return;
+
+    localStorage.setItem('username', this.form.get('username')?.value);
+    this.router.navigate(['/private/customers']);
+  }
+}
