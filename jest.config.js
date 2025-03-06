020f124ed2fae2ad060fa8e450a3c711c48cd802
Pedro Barros <am.pedrobarros@outlook.com>
2025-03-06 15:23:29 -0300

    fix: fix jest issues

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-  coverageDirectory: '<rootDir>/coverage/',
-  collectCoverage: true,
-  coverageReporters: ['html', 'text-summary'],
-};
